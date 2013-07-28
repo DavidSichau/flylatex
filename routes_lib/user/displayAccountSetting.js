e0b9ad70acaeff22e8e4b8c931ea4a4c1d2d31ab
DavidSichau <dsichau@gmail.com>
2013-07-28 06:16:21 -0400

    The current saved data is also show under the Account settings

--- routes_lib/user/displayAccountSetting.js
+++ routes_lib/user/displayAccountSetting.js
@@ -1,3 +1,9 @@
+var configs = require("../../configs")
+var mongoose = require("mongoose").connect(configs.db.url);
+var User = mongoose.model("User");
+
+
+
 /*
  * displayAccountSettings ->
  *  displays form for changing the account settings
@@ -5,17 +11,31 @@
  * @param req -> request object
  * @param res -> response object
  */
-var displayAccountSettings= function(req, res) {
-    res.render("account-settings", {
-        title: "Change you account information for Fly Latex",
-        shortTitle: "Account Settings",
-        tagLine: "of " + req.session.currentUser,
-        currentUser: req.session.currentUser,
-        isLoggedIn: req.session.isLoggedIn,
-        userDocuments: req.session.userDocuments,
-        fileSpecificStyle: "sign-up.css",
-        fileSpecificScript: "application.js"
+var displayAccountSettings = function(req, res) {
+    User.findOne({
+        userName: req.session.currentUser
+    }, function(err, dbUser) {
+        res.render("account-settings", {
+            title: "Change you account information for Fly Latex",
+            shortTitle: "Account Settings",
+            tagLine: "of " + req.session.currentUser,
+            currentUser: req.session.currentUser,
+            email: dbUser.email,
+            firstName: dbUser.firstName,
+            lastName: dbUser.lastName,
+            isLoggedIn: req.session.isLoggedIn,
+            userDocuments: req.session.userDocuments,
+            fileSpecificStyle: "sign-up.css",
+            fileSpecificScript: "application.js"
+        });
+
+
     });
+    
+    
+    
+    
+
 };
 
 module.exports = displayAccountSettings;
